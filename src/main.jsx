2830e98afbcf52b3fd89651e6ca46233ef4cb9e4
Julie <lerchejulie@gmail.com>
2024-07-04 13:29:58 -0500

    implementing hooks to change the quantity of items

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,77 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
+import { useState } from 'react';
 
-function Header() {
+const items = [
+    {
+      name: "Apples",
+      quantity: 5,
+      id: 1
+    },
+    {
+      name: "Bananas",
+      quantity: 7,
+      id: 2
+    },
+    {
+      name: "Box of Pasta",
+      quantity: 1,
+      id: 3
+    },
+    {
+      name: "Cookies",
+      quantity: 12,
+      id: 4
+    }
+  ]
+
+function Header({title, itemTotal}) {
     return (
         <header>
-            <h1>Grocery List</h1>
-            <span className='total-items'>Items: 1</span>
+            <h1>{title}</h1>
+            <span className='total-items'>Items: {itemTotal.length}</span>
         </header>
     )
 }
 
-const Item = () => {
+const Item = ({itemName}) => {
     return (
         <div className='item'>
             <button className='remove-item' />
-            <span className='item-name'>Apples</span>
+            <span className='item-name'>{itemName}</span>
             <Counter />
         </div>
     )
 }
 
 const Counter = () => {
+    const [itemQuantity, setQuantity] = useState(0)
+
+    const incrementQuantity = () => {
+        console.log("hi, from inside incrementQuantity")
+    }
+
     return (
         <div className='quantity'>
                 <span className='qty-label'>QTY</span>
                 <button className='increment'>+</button>
                 <button className='decrement'>-</button>
-                <span className='quantity-amount'>15</span>
+                <span className='quantity-amount'>{itemQuantity}</span>
             </div>
     )
 }
 
-const App = () => {
+const App = ({initalList}) => {
     return (
         <div className='grocery-list'>
-            <Header />
+            <Header title='Grocery List' itemTotal={initalList}  />
             {/* Grocery LIst */}
-            <Item />
-
+            {initalList.map(item =>
+                <Item 
+                itemName={item.name} 
+                key={item.id}
+                />
+            )}
         </div>
     )
 }
@@ -45,6 +79,6 @@ const App = () => {
 const root = createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-    <App />
+    <App initalList={items} />
     </StrictMode>
 );
